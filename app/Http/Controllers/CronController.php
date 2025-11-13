f4f0db8063c8cd52c8f0b3f072c9226fc7f3ef2c
atikur-webdev <atikur.codes@gmail.com>
2025-11-13 12:33:05 +0600

    initial commit

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Installment;
+use App\Models\Transaction;
 use App\Models\User;
 use App\Models\UserDps;
 use Carbon\Carbon;
@@ -14,36 +15,105 @@ class CronController extends Controller
     {
         $this->checkDate();
     }
+
     public function checkDate()
     {
-        // $today = Carbon::now();
-        $userId = auth()->id();
-
-        $userDps = UserDps::with('user')->where('user_id', $userId)->get();
+        $userDps = UserDps::with('user')->get();
 
         foreach ($userDps as $dps) {
+            $installment = Installment::where('user_id', $dps->user_id)->where('user_dps_id', $dps->dps_id)->latest()->first();
 
-            $installments = Installment::where('user_id', $dps->user_id)->where('dps_id', $dps->dps_id)->first();
-            $installmentDate = $installments->payment_date;
-            $today = now();
-
-            // $curr_date = $today->format('Y-m-d');
-
-            $next30Days = $today->copy()->addDays(30);
-
-            // dd($installmentDate, $next30Days->format('Y-m-d'));
-
-            if (Carbon::parse($installmentDate)->format('Y-m-d') == $next30Days->format('Y-m-d')) {
-                info('hoice');
-                $dps->user->balance -= $dps->per_installment;
-                $dps->user->save();
-                $installments->amount += $dps->per_installment;
-                $installments->total_installment += 1;
-                $installments->save();
+            if (empty($installment)) {
+                continue;
             }
+            
+            $user = $dps->user;
+            $installmentDate = Carbon::parse($dps->curr_date);
+            $nextInstallmentDate = carbon::parse($dps->curr_date)->addDays($dps->installment_interval);
+
+            if ($installmentDate <= now() && $user->balance > $dps->per_installment) {
+                $user->balance -= $dps->per_installment;
+                $user->save();
+                $transaction = new Transaction();
+                $transaction->user_id = $user->id;
+                $transaction->amount = $dps->per_installment;
+                $transaction->type = '-';
+                $transaction->post_balance = $user->balance;
+                $transaction->details = 'Dps amount subtracted';
+                $transaction->remarks = 'dps_amount_subtracted';
+                $transaction->trx = trxGenerator();
+                $transaction->save();
+
+                $interest = $dps->per_installment * 10 / 100;
+                $user->balance += $interest;
+                $user->save();
+                $transaction = new Transaction();
+                $transaction->user_id = $user->id;
+                $transaction->amount = $interest;
+                $transaction->type = '+';
+                $transaction->post_balance = $user->balance;
+                $transaction->details = 'Per dps interest added';
+                $transaction->remarks = 'per_dps_added';
+                $transaction->trx = trxGenerator();
+                $transaction->save();
+
+                // increment installment and change date
+                $installment = new Installment();
+                $installment->user_id       = $user->id;
+                $installment->user_dps_id   = $dps->id;
+                $installment->amount = $dps->per_installment;
+                $installment->total_installment = 1;
+                $installment->payment_date = now()->format('Y-m-d');
+                $installment->save();
+
+                $dps->given_installment += 1;
+                $dps->curr_date = $nextInstallmentDate;
+                $dps->save();
+
+            };
+
         }
     }
 }
+
+
+
+// foreach ($userDps as $dps) {
+    // $installments = Installment::where('user_id', $dps->user_id)->where('dps_id', $dps->dps_id)->first();
+    // $installmentDate = $installments->payment_date;
+    // $today = now()->startOfDay();
+    // $lastPaymentDate = Carbon::parse($installments->payment_date)->startOfDay();
+    // $dayPassed = $today->diffInDays($lastPaymentDate, false);
+
+    // if($dayPassed >= 30) {
+    //     $dps->user->balance -= $dps->per_installment;
+    //     $dps->user->save();
+    //     $installments->amount += $dps->per_installment;
+    //     $installments->total_installment += 1;
+    //     $installments->save();
+    // }
+// }
+
+
+
+
+            // $curr_date = $today->format('Y-m-d');
+
+            // $next30Days = $today->copy()->addDays(30);
+
+            // dd($installmentDate, $next30Days->format('Y-m-d'));
+
+            // if (Carbon::parse($installmentDate)->format('Y-m-d') == $next30Days->format('Y-m-d')) {
+            //     info('hoice');
+            //     $dps->user->balance -= $dps->per_installment;
+            //     $dps->user->save();
+            //     $installments->amount += $dps->per_installment;
+            //     $installments->total_installment += 1;
+            //     $installments->save();
+            // }
+//         }
+//     }
+// }
 
         // foreach ($ as $date) {
         //     $user = User::where('id', auth()->id())->firstOrFail();
